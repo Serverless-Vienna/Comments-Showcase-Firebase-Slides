886c21fe45bc36d54ab32b643aa43d74bf4a1eba
László Király <laszlo.t.kiraly@gmail.com>
2017-04-28 00:17:42 +0200

    updates from talk

--- presentation/index.js
+++ presentation/index.js
@@ -16,12 +16,8 @@ import {
   Slide,
   Text,
   CodePane,
-  Code,
   Appear,
-  Fit,
-  Fill,
-  Image,
-  Markdown
+  Image
 } from "spectacle";
 
 // Import image preloader util
@@ -37,7 +33,10 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   awsDiagram: require("../assets/comment-showcase-block-diagram-aws.png"),
-  blow: require("../assets/blow.gif")
+  onWriteTimeUpper: require("../assets/onWriteTimeUpper.gif"),
+  firebaseWebapp: require("../assets/webapp.gif"),
+  invocations: require("../assets/invocations.png"),
+  restTimestampOnWriteUpper: require("../assets/restTimestamp-onWriteUpper.gif")
 };
 
 preloader({ images });
@@ -76,17 +75,21 @@ export default class Presentation extends React.Component {
             <Appear><ListItem>to make it work</ListItem></Appear>
             <Appear><ListItem>to make it work right</ListItem></Appear>
             <Appear><ListItem>to make it work fast</ListItem></Appear>
-            <Appear><ListItem><b>to make it scalable</b></ListItem></Appear>
+            <Appear><ListItem><b>to make it serverless scalable</b></ListItem></Appear>
           </List>
           <Heading size={4} fit textFont="primary" textColor="secondary" caps>Writing Code</Heading>
+        </Slide>
+        <Slide transition={["spin", "fade"]} bgColor="white" >
+          <Image src={images.firebaseWebapp.replace("/", "")} margin="0px" height="600px" />
+          <Heading size={4} textFont="primary" textColor="secondary" caps>The Frontend</Heading>
         </Slide>
         <Slide transition={["spin", "fade"]} bgColor="white" >
           <Image src={images.awsDiagram.replace("/", "")} margin="0px auto 10px" />
           <Heading size={4} fit textFont="primary" textColor="secondary" caps>AWS - Comments Showcase</Heading>
         </Slide>
         <Slide transition={["slide"]}>
-          <CodePane source={require("raw-loader!../assets/code/firebase-23.ascii")}/>
-          <Heading size={4} textFont="primary" textColor="secondary" caps>Firebase - Comments Showcase</Heading>
+          <CodePane source={require("raw-loader!../assets/code/result.ascii")}/>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Firebase - Comments Showcase</Heading>
           {/* <Heading size={4} fit textFont="primary" textColor="secondary" caps>a case for upper</Heading> */}
         </Slide>
         <CodeSlide
@@ -113,30 +116,65 @@ export default class Presentation extends React.Component {
              { loc: [1, 2], title: "Uppercasing" },
              { loc: [2, 3], title: "Uppercasing" },
              { loc: [4, 8], title: "Uppercasing" },
-             { loc: [7, 8], title: "Uppercasing", note: "Timeout bug: http://stackoverflow.com/questions/43151022/firebase-cloud-function-onwrite-timeout" }
+             { loc: [7, 8], title: "Uppercasing", note: "Timeout bug: http://stackoverflow.com/questions/43151022/firebase-cloud-function-onwrite-timeout probably fixed by now" }
           ]}
         />
         <Slide transition={["slide"]}>
-          <CodePane source={require("raw-loader!../assets/code/firebase-servertime.ascii")}/>
-          <Heading size={4} fit textFont="primary" textColor="secondary" caps>let's add the servertime</Heading>
+          <CodePane source={require("raw-loader!../assets/code/firebase-timestamp.ascii")}/>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>let's add the timestamp</Heading>
         </Slide>
         <CodeSlide transition={[]}
           lang="js"
-          code={require("raw-loader!../assets/code/servertime.code")}
+          code={require("raw-loader!../assets/code/timestamp.code")}
           ranges={[
-             { loc: [0, 12], title: "Adding Servertime" },
-             { loc: [1, 3], title: "Adding Servertime" },
-             { loc: [4, 9], title: "Adding Servertime" },
-             { loc: [6, 9], title: "Adding Servertime", note: "Can you spot the problem?" }
+             { loc: [0, 12], title: "Adding Timestamp" },
+             { loc: [1, 3], title: "Adding Timestamp" },
+             { loc: [4, 9], title: "Adding Timestamp" },
+             { loc: [6, 9], title: "Adding Timestamp", note: "Can you spot the problem?" }
           ]}
         />
+        <Slide transition={[]} bgColor="white">
+          <Heading size={5} textFont="primary" textColor="secondary" caps>infinite loop!</Heading>
+          <Image src={images.invocations.replace("/", "")} margin="0px" height="600px"/>
+        </Slide>
         <CodeSlide
           lang="js"
-          code={require("raw-loader!../assets/code/servertime-guard.code")}
+          code={require("raw-loader!../assets/code/timestamp-guard.code")}
           ranges={[
-             { loc: [6, 13], title: "Adding Guard", note: "No more recursion!" }
+             { loc: [6, 13], title: "Adding Guard", note: "No more infinite loop!" }
           ]}
         />
+        <Slide transition={[]} bgColor="white">
+          <Heading size={5} textFont="primary" textColor="secondary" caps>push in action</Heading>
+          <Image src={images.onWriteTimeUpper.replace("/", "")} margin="0px" height="600px"/>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <CodePane lang="java" source={require("raw-loader!../assets/code/firebase-timestamp-uppercase.ascii")}/>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>let's use REST</Heading>
+        </Slide>
+        <CodeSlide
+          lang="js"
+          code={require("raw-loader!../assets/code/client-firebase-rest.code")}
+          ranges={[
+             { loc: [0, 17], title: "Client Side Post" },
+             { loc: [2, 4], title: "Client Side Post" },
+             { loc: [5, 12], title: "Client Side Post" },
+             { loc: [13, 14], title: "Client Side Post" }
+          ]}
+        />
+        <Slide transition={[]} bgColor="white">
+          <Heading size={5} textFont="primary" textColor="secondary" caps>REST in action</Heading>
+          <Image src={images.restTimestampOnWriteUpper.replace("/", "")} margin="0px" height="600px"/>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <CodePane lang="java" source={require("raw-loader!../assets/code/firebase-uppercase-inbox.ascii")}/>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Or use inbox</Heading>
+        </Slide>
+        <Slide transition={["slide"]}>
+          <CodePane source={require("raw-loader!../assets/code/result.ascii")}/>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Firebase - Comments Showcase</Heading>
+          {/* <Heading size={4} fit textFont="primary" textColor="secondary" caps>a case for upper</Heading> */}
+        </Slide>
         <CodeSlide
           lang="js"
           code={require("raw-loader!../assets/code/database-rules.json")}
@@ -156,16 +194,6 @@ export default class Presentation extends React.Component {
         />
         <CodeSlide
           lang="js"
-          code={require("raw-loader!../assets/code/client-firebase-rest.code")}
-          ranges={[
-             { loc: [0, 17], title: "Client Side Post" },
-             { loc: [2, 4], title: "Client Side Post" },
-             { loc: [5, 12], title: "Client Side Post" },
-             { loc: [13, 14], title: "Client Side Post" }
-          ]}
-        />
-        <CodeSlide
-          lang="js"
           code={require("raw-loader!../assets/code/client-firebase-rest-auth.code")}
           ranges={[
             { loc: [0, 19], title: "Post Auth" },
@@ -174,14 +202,48 @@ export default class Presentation extends React.Component {
             { loc: [15, 16], title: "Post Auth" }
           ]}
         />
-        <Slide transition={["zoom", "fade"]} >
-          <Image src={images.blow.replace("/", "")} margin="40px auto" />
-          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Questions?</Heading>
+        <Slide transition={["fade", "zoom", "slide"]}>
+          <List textFont="secondary" textColor="tertiary">
+            <Appear><ListItem>Functions is a node project</ListItem></Appear>
+            <Appear><ListItem>Authentication built in</ListItem></Appear>
+            <Appear><ListItem>Has npm client libs</ListItem></Appear>
+            <Appear><ListItem>Deploy hosting, functions, database only</ListItem></Appear>
+          </List>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Developer Experience: The good</Heading>
+        </Slide>
+        <Slide transition={["fade", "zoom", "slide"]}>
+          <List textFont="secondary" textColor="tertiary">
+            <Appear><ListItem>Slow Deployment (minutes not seconds)</ListItem></Appear>
+            <Appear><ListItem>No "before" write function</ListItem></Appear>
+          </List>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Developer Experience: The bad</Heading>
+        </Slide>
+        <Slide transition={["fade", "zoom", "slide"]}>
+          <List textFont="secondary" textColor="tertiary">
+            <Appear><ListItem>Randomly failing Deployments</ListItem></Appear>
+            <Appear><ListItem>Never ending Deployment</ListItem></Appear>
+            <Appear><ListItem>Timeout Bug $$$</ListItem></Appear>
+          </List>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>Developer Experience: The ugly</Heading>
+        </Slide>
+        <Slide transition={["fade", "zoom", "slide"]}>
+          <List textFont="secondary" textColor="tertiary">
+            <Appear><ListItem>Testing functions</ListItem></Appear>
+            <Appear><ListItem>Groups and authorization</ListItem></Appear>
+            <Appear><ListItem>Providers as plugins</ListItem></Appear>
+            <Appear><ListItem>Offline/Sync Capabilities</ListItem></Appear>
+          </List>
+          <Heading size={4} fit textFont="primary" textColor="secondary" caps>What's next</Heading>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <Heading size={2} textColor="secondary" caps>Save the Dates</Heading>
           <Heading size={4} fit margin="40px 0px" textColor="tertiary" caps>May 10th, Coding Session, Stockwerk</Heading>
           <Heading size={4} fit margin="40px 0px" textColor="tertiary" caps>May 15th, Serverless and Microservices</Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>Links</Heading>
+          <Heading size={4} fit margin="40px 0px" textColor="tertiary" caps>https://github.com/Serverless-Vienna/Comments-Showcase/tree/firebase</Heading>
+          <Heading size={4} fit margin="40px 0px" textColor="tertiary" caps>https://serverless-vienna.firebaseapp.com/</Heading>
         </Slide>
       </Deck>
     );
